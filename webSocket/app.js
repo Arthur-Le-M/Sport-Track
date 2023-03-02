760ee2f42e9822f4c0cafcb022e347ef3ffe2fb7
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-03-02 12:21:05 +0100

    Fonction javascript de websocket

--- webSocket/app.js
+++ webSocket/app.js
@@ -1,2 +1,32 @@
 var conn = new WebSocket('ws://193.55.218.254:8080');
 
+conn.onopen = function(e) {
+    console.log("Connection ouvert!");
+};
+
+conn.onmessage = function(e) {
+    //Récupération du JSON
+    var data = JSON.parse(e.data);
+    //Ajouter le message en html sur la page
+};
+
+conn.onclose = function(e) {
+    console.log("Connection fermé!");
+}
+
+function leaveChat(){
+
+    $.ajax({
+        url:"action.php",
+        method:"post",
+        data: "userId="+userId+"&action=leave"
+    }).done(function(result){
+        var data = JSON.parse(result);
+        if(data.status == 1) {
+            conn.close();
+            location = "index.php";
+        } else {
+            console.log(data.msg);
+        }		
+    });
+}
