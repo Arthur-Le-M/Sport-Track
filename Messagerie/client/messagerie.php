4c1a39b51cd76a3d051e8c255dc559bbf3a6ac0d
mchabanat <matischabanat@gmail.com>
2023-03-03 14:25:08 +0100

    Rectification chemin vers les images pour header/footer et modid du front de la messagerie

--- Messagerie/client/messagerie.php
+++ Messagerie/client/messagerie.php
@@ -13,15 +13,15 @@ else{echo("id non-defini");}
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="messagerie.css">
+    <!-- Lien vers le CSS du header et du footer -->
+    <link rel="stylesheet" href="../Template/style.css">
     <title>Sport Track - Messagerie</title>
 </head>
 <body>
     <?php
     // Import du header (commun à toutes les pages)
-    // require "header.php";
+    require "../Template/header.php";
     ?>
-    <header></header>
-
     <main>
         <div id="modal">
             <div id="modalDesc">
@@ -49,7 +49,7 @@ else{echo("id non-defini");}
 
     <?php
     // Import du footer (commun à toutes les pages)
-    // require "footer.php";
+    require "../Template/footer.php";
     ?>
     <script src="script.js"></script>
     <!--<script src="script.js"></script>-->
