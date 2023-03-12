dc97865bb43f99b7ec2a430aec6930de4df715f6
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-03-12 14:58:50 +0100

    page rgpd

--- Information/rgpd.php
+++ Information/rgpd.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/politique-de-conf.css">
+    <link rel="stylesheet" href="./css/rgpd.css">
     <!-- Lien vers le CSS du header et du footer -->
     <link rel="stylesheet" href="../Template/style.css">
-    <script src="script-scroll.js"></script>
     <title>RGPD · Sport Track</title>
 </head>
 
@@ -16,79 +15,13 @@
     // Import du header (commun à toutes les pages)
     require "../Template/header.php";
     ?>
-    <main>
         <div id="titre">
-            <h2>Politique de confidentialité</h2>
-            <p class="date">Date de la dernière mise à jour : 08/03/2023</p>
+            <h2>RGPD</h2>
+            <p class="date">Date de la dernière mise à jour : 07/03/2023</p>
         </div>
-
-        <section id="introduction">
-            <p>Chez Sport Track, dès lors qu'il s'agit de vos données à caractère personnel, la sécurité et la transparence sont des priorités absolues. Afin de vous aider à comprendre quelles sont les informations que nous collectons à votre sujet, comment nous les utilisons et quels sont vos droits, nous avons rédigé la présente Politique de confidentialité détaillée dont en voici le plan :</p>
-            <ul>
-                <li><a href="#point1">Généralités</a></li>
-                <li><a href="#point2">Pourquoi et comment nous collectons et nous utilisons vos données</a></li>
-                <li><a href="#point3">Bénéficiaires des données à caractère personnel</a></li>
-                <li><a href="#point4">Utilisation de cookies</a></li>
-                <li><a href="#point5">Droit d'amendement</a></li>
-                <li><a href="#point6">Vos droits concernant vos données</a></li>
-                <li><a href="#point7">Nos coordonnées</a></li>
-            </ul>
-        </section>
-        <section id="developpement">
-            <article id="point1">
-                <h3>1. Généralités</h3>
-                <p>La présente Politique de confidentialité a pour objet de vous informer sur la manière dont Sport Track collecte et utilise vos données à caractère personnel. Elle s'applique à toutes les données à caractère personnel que nous collectons à votre sujet, qu'il s'agisse de données que vous nous communiquez directement ou de données que nous collectons automatiquement à votre sujet (tels que les cookies).</p>
-                <p>La présente Politique de confidentialité est rédigée en conformité avec le Règlement général sur la protection des données (RGPD) et la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.</p>
-                <p>Nous prenons le respect de votre vie privée très au sérieux. Toutes les données à caractère personnel seront collectées, stockées et utilisées par nos soins, conformément au Règlement général sur la protection des données n° 2016/679 de l'Union européenne (« RGPD »), et/ou à tout autre règlement statutaire en vigueur.</p>
-            </article>
-            <article id="point2">
-                <h3>2. Pourquoi et comment nous collectons et nous utilisons vos données</h3>
-                <p>Notre site web collecte des données à caractère personnel pour fournir des services personnalisés et améliorer votre expérience d'utilisation. Nous collectons ces données à travers différents moyens, notamment lorsque vous vous inscrivez à notre site web, lorsque vous utilisez nos services, lorsque vous participez à des enquêtes ou des sondages, ou lorsque vous interagissez avec notre site web d'une autre manière.</p>
-                <p>Nous utilisons ces données pour fournir des services de qualité supérieure, pour personnaliser votre expérience en fonction de vos préférences, pour résoudre les problèmes techniques liés à notre site web, pour améliorer la sécurité de notre site web et pour respecter les exigences légales et réglementaires. Nous ne vendrons pas vos données à des tiers et nous ne les utiliserons pas à des fins publicitaires sans votre consentement explicite.</p>
-            </article>
-            <article id="point3">
-                <h3>3. Bénéficiaires des données à caractère personnel</h3>
-                <p>Nous partageons ces données avec des tiers uniquement dans les cas où cela est nécessaire pour fournir les services que vous avez demandés, pour respecter les exigences légales et réglementaires, pour améliorer la sécurité de notre site web, ou lorsque vous avez donné votre consentement explicite. Nous travaillons uniquement avec des tiers qui ont des politiques de confidentialité solides et qui s'engagent à protéger vos données à caractère personnel.</p>
-                <p>Nous comprenons l'importance de la confidentialité de vos données à caractère personnel et nous nous engageons à les protéger. Nous avons mis en place des mesures de sécurité appropriées pour protéger vos données contre l'accès, l'utilisation ou la divulgation non autorisés. Nous avons également mis en place des procédures pour garantir que les données à caractère personnel sont conservées uniquement pendant la période nécessaire pour atteindre les objectifs pour lesquels elles ont été collectées.</p>
-            </article>
-            <article id="point4">
-                <h3>4. Utilisation de cookies</h3>
-                <p>Nous utilisons des cookies qui sont des petits fichiers de données stockés sur votre ordinateur lorsque vous visitez le site web. Ils sont utilisés pour stocker des informations telles que vos préférences et vos données de navigation afin de faciliter la navigation et d'améliorer votre expérience de l'utilisateur sur notre site.</p>
-                <p>Selon la loi française, les sites web doivent obtenir le consentement de l'utilisateur avant de stocker des cookies sur son ordinateur. En tant que propriétaire du site, nous sommes responsables d'informer clairement les utilisateurs de notre utilisation des cookies et de leur demander leur consentement. Il est important de noter que nous n'utilisons pas les cookies pour collecter des informations personnelles identifiables sans votre consentement. Nous devons donc demander clairement aux utilisateurs leur consentement de façon explicite avant de pouvoir collecter leurs données personnelles.</p>
-                <p>C'est pourquoi dès votre arrivée sur le site, nous vous laissons le choix d'accepter ou de refuser nos cookies grâce à un formulaire.</p>
-            </article>
-            <article id="point5">
-                <h3>5. Droit d'amendement</h3>
-                <p>Dans la mesure où nous développons constamment nos services, nous nous réservons le droit de modifier la présente Politique de confidentialité à tout moment, sous réserve des règlements applicables. Toute modification sera publiée sur cette page dans les meilleurs délais. Indépendamment de ce qui précède, vous devrez consulter cette page régulièrement pour toute mise à jour.</p>
-            </article>
-            <article id="point6">
-                <h3>6. Vos droits concernant vos données</h3>
-                <p>En vertu de la législation sur la protection des données, vous disposez de certains droits relatifs à vos données personnelles :</p>
-                <ul>
-                    <li>d'être informé(e) des données que nous collectons et utilisons et de demander l'accès aux données concernées ou d'en exiger une copie (droit d'être informé(e));</li>
-                    <li>sous réserve d'une cause juste, d'exiger la suppression de vos données (droit de suppression);</li>
-                    <li>d'exiger la restriction de la collecte et de l'utilisation de vos données, à condition que les critères statutaires soient respectés (droit de restreindre le traitement);</li>
-                    <li>de s'opposer à la collecte et à l'utilisation des données;</li>
-                    <li>de retirer à tout moment toute autorisation que vous nous avez fournie.</li>
-                </ul> 
-                <p>Pour exercer un quelconque droit spécifié dans la présente section, vous pouvez contacter Vinted et soumettre votre demande en utilisant les coordonnées (consulter la section 7 ci-dessous). Indépendamment de tout autre recours juridique, vous avez également le droit à tout moment de soumettre une plainte auprès des autorités de surveillance.</p>
-            </article>
-            <article id="point7">
-                <h3>7. Nos coordonnées</h3>
-                <p>Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité ou la manière dont nous traitons vos données personnelles, veuillez nous contacter aux adresses suivantes : </p>
-                <p><span>Adresse e-mail : </span>
-                <ul>
-                    <li>team@sport-track.live</li>
-                </ul></p>
-                <p><span>Adresse Postale : </span>
-                <ul>
-                    <li>2 avenue du parc Montaury</li>
-                    <li>64600 Anglet</li>
-                    <li>FRANCE</li>
-                </ul></p>
-            </article>
-        </section>
-        
+        <main>
+        <p class='rgpdP'>Le RGPD (Règlement Général sur la Protection des Données) est une réglementation européenne qui régit la protection des données personnelles des citoyens de l'Union Européenne. Ce règlement vise à renforcer les droits des individus en matière de protection de leurs données personnelles, ainsi qu'à responsabiliser les entreprises et organisations qui traitent ces données. Le RGPD contient des dispositions concernant la collecte, le traitement, la conservation et la destruction des données personnelles, ainsi que des mesures de sécurité à mettre en place pour protéger ces données contre les fuites, les pertes ou les piratages. Le RGPD impose également des sanctions en cas de non-respect de ces dispositions, afin de garantir une protection efficace des données personnelles.</p>
+        <a href="../Information/doc/RGPD.pdf" class="buttonDownload">Download</a>
     </main>
     <?php
     // Import du footer (commun à toutes les pages)
