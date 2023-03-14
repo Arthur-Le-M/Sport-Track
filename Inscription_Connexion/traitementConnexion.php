9b34325a3bf671369d97dfb099d9cce6bc1fac06
Titouan Cocheril <titouan.cocheril25@gmail.com>
2023-03-14 12:19:51 +0100

    Bug dans le fichier de connexion fixé

--- Inscription_Connexion/traitementConnexion.php
+++ Inscription_Connexion/traitementConnexion.php
@@ -10,7 +10,7 @@ require "../Template/config.php"; // Lien pour la connexion a la BD
 $conn = getConnection_Lecture();
 
 //On verifie que l'email est valide
-$req = "SELECT * FROM inscrit WHERE mail=:mail";
+$req = "SELECT * FROM Inscrit WHERE mail=:mail";
 $req = $conn->prepare($req);
 $req->execute(['mail'=>$mail]);
 $res = $req->fetchAll();
@@ -18,14 +18,14 @@ if(count($res)==1){
     //L'email est valide
     //LES VERIFICATION SONT SUCCINTE CAR C'EST UNE VERSION TEST DU PROJET
     //On vérifie que le mot de passe correspond à l'email
-    $req = "SELECT mdp FROM inscrit WHERE mail=:mail";
+    $req = "SELECT mdp FROM Inscrit WHERE mail=:mail";
     $req = $conn->prepare($req);
     $req->execute(['mail'=>$mail]);
     $res = $req->fetch();
     if(password_verify($passwd, $res['mdp'])){
         //Le mot de passe est bon
         //On ajoute l'id au variables de session // Lien pour la connexion a la BD
-        $recupID = $conn->prepare('SELECT id, licence FROM inscrit WHERE mail=? AND mdp=?');
+        $recupID = $conn->prepare('SELECT id, licence FROM Inscrit WHERE mail=? AND mdp=?');
         $recupID->execute([$mail,$res['mdp']]);
         //array($_SESSION['id'])
         $resultat = $recupID->fetch();
